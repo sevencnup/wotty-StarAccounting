14de15bc6aaf9c7f19200a5740b3e7e921221503
xingseven <05110tv@gmail.com>
2026-07-29 21:41:19 +0800

    chore: bump wotty stark to 0.0.34

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wotty.stark"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "0.0.33"
+        versionCode = 34
+        versionName = "0.0.34"
     }
 
     buildTypes {
